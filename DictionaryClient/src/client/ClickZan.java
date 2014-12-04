f6ce56ea9dab6a01bd3509b3d5e54449f5a6028d
tzzcl <zclnjucs@gmail.com>
2014-12-04 10:50:59 +0800

    Some Client Code Signed-off-by: tzzcl <zclnjucs@gmail.com>

--- DictionaryClient/src/client/ClickZan.java
+++ DictionaryClient/src/client/ClickZan.java
@@ -1,5 +1,14 @@
 package client;
 
-public class ClickZan {
+import java.io.IOException;
+import java.net.UnknownHostException;
 
+public class ClickZan {
+	public void clickZan(String word,String which) throws UnknownHostException, IOException{
+		String user=Main.getUser();
+		String content="zan";
+		content=content+" "+word+" "+which;
+		Thread thread=new Thread(new SendToServer(Main.getIP(), content));
+		thread.start();
+	}
 }
--- DictionaryClient/src/client/DealAnswer.java
+++ DictionaryClient/src/client/DealAnswer.java
@@ -2,6 +2,43 @@ package client;
 
 public class DealAnswer {
 	public void deal(String content){
-		
+		if (content.contains("Register")){
+			if (content.equals("Register Success!")){
+				//Msg Box
+			}
+			else{
+				//Msg Box
+			}
+		}
+		if (content.contains("Login")){
+			if (content.equals("Login Success!")){
+				//Msg Box
+			}
+			else{
+				//Msg Box
+			}
+		}
+		if (content.contains("Zan")){
+			if (content.equals("Zan Success!")){
+				//Msg Box
+			}
+			else{
+				//Msg Box
+			}
+		}
+		if (content.contains("Cancel")){
+			if (content.equals("Cancel Success!")){
+				//Msg Box
+			}
+			else{
+				//Msg Box
+			}
+		}
+		if (content.equals("No such words!")){
+			
+		}
+		else{
+			//Set Explaination
+		}
 	}
 }
