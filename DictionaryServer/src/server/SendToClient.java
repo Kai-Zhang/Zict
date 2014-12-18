74f66fc5ad27df6dcc331bdf43a24e81f1ed8aa7
tzzcl <zclnjucs@gmail.com>
2014-12-18 09:44:21 +0800

    Add Query share word Function Signed-off-by: tzzcl <zclnjucs@gmail.com>

--- DictionaryServer/src/server/SendToClient.java
+++ DictionaryServer/src/server/SendToClient.java
@@ -27,7 +27,7 @@ public class SendToClient implements Runnable{
 			//printWriter.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		
 	}
